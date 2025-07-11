efb0a9c90847f2eb3447d0d7aa0dca2e7b7cc734
surajpandey111 <125928082+surajpandey111@users.noreply.github.com>
2025-07-11 11:05:11 +0530

    SEO updates with custom domain

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
   return (
