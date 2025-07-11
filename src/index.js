dd1eb86127e824ac8dff0113aca461a98303e14a
surajpandey111 <125928082+surajpandey111@users.noreply.github.com>
2025-07-11 08:28:25 +0530

    Updated to react-router-dom v6

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Contact from "./Contact";
-import Projects from "./Projects";
-import Skills from "./Skills";
-import About from "./About";
-import Timeline from "./Timeline";
-import AdminPosts from "./AdminPosts";
-import NotFound from "./NotFound";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/skills" element={<Skills />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/timeline" element={<Timeline />} />
-      <Route path="/admin-posts" element={<AdminPosts />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Router>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+reportWebVitals();
